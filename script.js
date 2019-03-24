3e8eb964e65a6f1fc30e2bc014b2243f140ac2c6
Vebol <vibol.ra@gmail.com>
2019-03-23 22:00:31 -0400

    3rd and final build
    Code cleaned up with comments

--- script.js
+++ script.js
@@ -1,107 +1,87 @@
-
+//function to find information of entered book name after clicking submit button
 function findBook(){
-
+     
    let input = document.getElementById('searchBarInput').value;
    let url = "https://www.googleapis.com/books/v1/volumes?q=" + input;
-
-
+      //Begin accessing JSON data here with AJAX
    let xhttp = new XMLHttpRequest();
    xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-
-       books = JSON.parse(this.responseText);
-
-            myBooks = [];
-                books.items.forEach(function(item) {
-
-                  let temp = new Book(item.volumeInfo.title,
-                                      item.volumeInfo.description,
-                                      item.volumeInfo,
-                                      item.saleInfo,
-                                      item.volumeInfo.authors);
-
-               myBooks.push(temp);
-
-});
-      displayResults();
-     
-
-      console.log(JSON.parse(this.responseText));
-
-    }
-  };
-
-  xhttp.open("GET", url, true);
-  xhttp.send();
-
-}
-
-
-function Book (title, description, thumbnail, price, authors){
-  input = document.getElementById('searchBarInput').value;
-
-  this.title = title;
-  this.description = function (){
-      
-      if(description !== undefined){
-    return description
-            .split(input)
-              .join("<strong>"+input.toUpperCase()+"</strong>");
-            }else {
-              return "Description unavailable"
+         if (this.readyState == 4 && this.status == 200) {
+              books = JSON.parse(this.responseText);
+              makeBook();
+              displayResults();
             }
-            }; 
-
-  this.thumbnail = function(){
-      
-      if(thumbnail.imageLinks !== undefined){
-        return "<img src=" + thumbnail.imageLinks.smallThumbnail + ">";
-      } else {
-        return "<img src=" + "http://p16cdn4static.sharpschool.com/UserFiles/Servers/Server_243551/Image/image%20not%20available.png" + ">";
-      }
-
-  };
-
-
-  this.price = function (){
-    if (price.listPrice !== undefined && price.listPrice.amount !== undefined){
-            return price.listPrice.amount ;
-          } else{
-            return "Not Available"
-          }
-  };
-  
-
-
-  this.authors = authors;
-
+     };
+    xhttp.open("GET", url, true);
+    xhttp.send();
 }
 
 
-let myBooks = [];
+function makeBook(){
+ //defining the array
+  myBooks = [];
+     //creates new book object for each item
+    books.items.forEach(function(item) {
+      let temp = new Book(item.volumeInfo.authors,
+                          item.volumeInfo.title,
+                          item.volumeInfo.description,
+                          item.volumeInfo,
+                          item.saleInfo);
+      myBooks.push(temp);
+
+              });
+}
 
 
+//constructor for the book
+function Book (authors, title, description, thumbnail, price){
+  input = document.getElementById('searchBarInput').value;
+  //returning data of each keys
+  this.authors = authors;
+  this.title = title;
+  this.description = () => {
+      if(description !== undefined){
+        //Also returning searched word but highlighted in description
+        return description
+          .split(input)
+          .join("<strong>"+input.toUpperCase()+"</strong>");   
+        } return "Description unavailable"
 
+     }; 
 
+  this.thumbnail = () => {     
+      if(thumbnail.imageLinks !== undefined){
+         // return image thumbnail of book
+        return "<img src=" + thumbnail.imageLinks.smallThumbnail + ">";
+       } //return default image thumbnail if smallThumbnail = undefined
+        return "<img src=" + "http://p16cdn4static.sharpschool.com/UserFiles/Servers/Server_243551/Image/image%20not%20available.png" + ">";
+     };
+       
+  this.price = () => {
+    if (price.listPrice !== undefined && price.listPrice.amount !== undefined){
+        //return value of price
+        return price.listPrice.amount ;
+          }// returning value if undefined 
+          return "Not Available"       
+      };
+  
+}
+
+//Showing the results in html function
 function displayResults(){
-    let result = '';
-      myBooks.forEach((book) => {
-
-        if (book.title !== undefined){
+   let result = '';
+   //Looping results in html
+    myBooks.forEach((book) => {
+       if (book.title !== undefined){
              result +=
-               '<li>' + "Title of Book: " + book.title + '</li>' +
-                  "Authors: "+ (book.authors) + "</br>" +
-                    "Price: "+ (book.price()) + "</br>" +
-                    "Description: "+ (book.description()) +
-                     book.thumbnail();
-
-                 }});
-                       document.getElementById("o1").innerHTML = result;
-
-        }
-
-
-
-
+               '<h1>' + "Title of Book: " + book.title + '</h1>' +
+               "Authors: "+ (book.authors) + "</br>" +
+               "Price: "+ (book.price()) + "</br>" +
+               "Description: "+ (book.description()) +
+                book.thumbnail();
+            }
+        });
+     document.getElementById("o1").innerHTML = result;
+}
 
 
